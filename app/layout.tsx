9a03c48c3e5b9e8adb90f48ae598c5ddd89bdfd5
Panda <armaan.ani@gmail.com>
2025-02-02 02:25:35 +0530

    Add login and signup form

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { ThemeProvider } from "@/components/themeProvider";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -12,8 +14,21 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang='en'>
-			<body className={`antialiased`}>{children}</body>
+		<html
+			lang='en'
+			suppressHydrationWarning
+		>
+			<body className='relative antialiased'>
+				<ThemeProvider
+					attribute='class'
+					defaultTheme='system'
+					enableSystem
+					disableTransitionOnChange
+				>
+					<Navbar />
+					{children}
+				</ThemeProvider>
+			</body>
 		</html>
 	);
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
 	return (
 		<div>
 			<h1>Hello</h1>
-			<Button>Hello Me</Button>
 		</div>
 	);
 }
